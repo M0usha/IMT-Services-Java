2e524e0ddd4177f5fdd0f644a644a7027ba1724f
Anael CHARDAN <anael.chardan@gmail.com>
2017-04-07 10:54:50 +0200

    TP1: Add serveurStateful

---
+++ tp1ServeurStateful/src/main/java/rest/Automate.java
@@ -0,0 +1,25 @@
+package rest;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+public interface Automate {
+
+	@POST
+	@Path("etat/initial")
+	@Produces(MediaType.APPLICATION_JSON)
+	Session initier();
+
+	@POST
+	@Path("etat/suivant/{lettre}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	Resultat accepter(@PathParam("lettre") char x, Session id);
+
+	@PUT
+    @Path("fin")
+	@Consumes(MediaType.APPLICATION_JSON)
+	void clore(Session id);
+
+}
+
